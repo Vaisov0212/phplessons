7e5fe416c28f6dc370fc125ba2c741ecf7ee962b
Azamat713 <azamwer.1999@gmail.com>
2026-02-19 16:25:02 +0500

    dizayn yuklandi

---
+++ db/connect.php
@@ -0,0 +1,13 @@
+<?php
+$server_name="localhost";
+$user_name="root";
+$password="";
+$db_name="tech_db";
+
+$conn=new mysqli($server_name,$user_name,$password,$db_name);
+if($conn->connect_error){
+    die("Ulanishda xatolik:".$conn->connect_error);
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require('db/connect.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="row">
+        <div class="col-sm-8">
+            <table class="table table-striped">
+            <thead>
+                 <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>email</th>
+                    <th>yosh</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+            
+        </table>
+        </div>
+    </div>
+
+        
+
+        
+</body>
+</html>
